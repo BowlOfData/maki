191c342c7a34fcb1c1ffb4d2ea60e85f5d2376b9
Marco <bowlofdata@gmail.com>
2025-05-11 12:47:52 +0200

    some test

--- maki/examples/ollama_examples.py
+++ maki/examples/ollama_examples.py
@@ -3,9 +3,9 @@ import sys
 from pathlib import Path
 sys.path.append(str(Path(__file__).parent.parent.parent))
 
-import maki
+from maki import Maki
 
-ma = maki.Maki("http://localhost", "11434", "gemma3:27b", temperature=0)
+ma = Maki("http://localhost", "11434", "gemma3:27b", temperature=0)
 
 result = ma.request("test")
 
